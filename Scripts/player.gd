289ffcbbca48826d2e2b6aaa94dc791afd3cfe91
riidans <109998029+riidans@users.noreply.github.com>
2026-01-31 18:06:45 -0500

    Add lerp to player movement to smooth it out

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -48,10 +48,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_just_pressed("slide") and is_on_floor():
-		ACCELERATION = 1.0
-		speed_modifier = 2.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
