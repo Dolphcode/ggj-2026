113c3da02ce2828ce840bf8f92efd453223aa07c
riidans <109998029+riidans@users.noreply.github.com>
2026-01-31 15:09:28 -0500

    Add debug panel, start state machine for sliding

---
+++ Scenes/debug.gd
@@ -0,0 +1,36 @@
+extends PanelContainer
+
+@onready var property_container = %VBoxContainer
+
+#var property
+var frames_per_second : String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+	global.debug = self
+	
+
+func _input(event):
+	# Toggle debug
+	if event.is_action_pressed("debug"):
+		visible = !visible
+
+func _process(delta):
+	if visible: 
+		frames_per_second = "%.2f" % (1.0/delta)
+		#property.text = property.name + ": " + frames_per_second
+		global.debug.add_property("FPS", frames_per_second, 2)
+		
+func add_property(title: String, value, order):
+	var target 
+	target = property_container.find_child(title, true, false)
+	if !target: 
+		target = Label.new()
+		property_container.add_child(target)
+		target.name = title
+		target.text = target.name + ": " + str(value)
+	elif visible:
+		target.text = title + ": " + str(value)
+		property_container.move_child(target, order)
