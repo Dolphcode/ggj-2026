33517e34237b27aef67aede59dc7f0a35fc4ee85
BuildTools <unconfigured@null.spigotmc.org>
2026-01-31 15:34:32 -0500

    Mask cameras

--- Scripts/Masks/fire_mask.gd
+++ Scripts/Masks/fire_mask.gd
@@ -10,15 +10,18 @@ class_name fire_mask
 
 #Called on equpping fire mask
 func Enter():
-	#mask_time = 10 #short
+	get_parent().mask_time = 10 #short
 	#gun_type = 'flamethrower'
 	mask.player.speed_modifier = 0.1 #10% speed
+	mask.player.get_node("Neck/Camera3D").fov = 20.0
 #Called on fire mask timeout/new mask equipped
 func Exit():
+	mask.player.speed_modifier = 1.0
+	mask.player.get_node("Neck/Camera3D").fov = 75.0
 	pass
 	
 func Update(delta: float):
-	get_parent().mask_update(delta)
+	get_parent().mask_update(delta, self)
 		
 func Physics_Update(_delta:float):
 	pass		
--- Scripts/Masks/radar_mask.gd
+++ Scripts/Masks/radar_mask.gd
@@ -1,12 +1,14 @@
 extends State
 class_name radar_mask
 
-
+@onready var mask = get_parent().get_parent()
+@onready var cam = mask.player.get_node("Neck/Camera3D")
 
 #Called on equpping radar_mask mask
 func Enter():
-	pass
-	#mask_time = 20 #medium
+	get_parent().mask_time = 20 #medium
+	#cam.rotate_x(-90)
+	#cam.position.y += 10.0
 	#gun_type = 'normal'
 	
 #Called on radar_mask mask timeout/new mask equipped
@@ -14,7 +16,7 @@ func Exit():
 	pass
 	
 func Update(delta: float):
-	get_parent().mask_update(delta)
+	get_parent().mask_update(delta, self)
 		
 func Physics_Update(_delta:float):
 	pass		
