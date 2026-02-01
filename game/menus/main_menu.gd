a536c9bbf12c9963377db02f89c2a45650a2970d
jds-472 <jds472@scarletmail.rutgers.edu>
2026-02-01 12:20:19 -0500

    HUD and menu additions and fixes

--- game/menus/main_menu.gd
+++ game/menus/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_button_up() -> void:
-		get_tree().change_scene_to_file("res://game/map.tscn")
+		get_tree().change_scene_to_file("res://game/main_game.tscn")
 
 
 func _on_story_button_button_up() -> void:
--- game/player/player.gd
+++ game/player/player.gd
@@ -125,4 +125,5 @@ func _on_retry_button_button_up() -> void:
 
 
 func _on_menu_button_button_up() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://game/menus/main_menu.tscn")
