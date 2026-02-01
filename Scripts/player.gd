7f00beb72bd8816d15fa9bb8666c08203144d52b
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2026-01-31 19:51:23 -0500

    Implemented sliding

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,15 +7,18 @@ class_name Player
 @export var GRAVITY_MODIFIER = 2.0
 @export var ACCELERATION_DEFAULT = 0.1
 @export var DECELERATION_DEFAULT = 1.0
+@export var DECELERATION_SLIDE = 0.5
 @export var ACCELERATION = ACCELERATION_DEFAULT
 @export var DECELERATION = DECELERATION_DEFAULT
+@export var SLIDE_BOOST = 20.0
 
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
 
 var speed_modifier := 1.0
 var player_velocity
-
+var sliding: bool = false
+var slide_transition: Tween = null
 
 @onready var slide_check = $SlideCheck
 
@@ -37,6 +40,26 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * SENSITIVITY)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("slide"):
+		speed_modifier = 0.0
+		velocity += (neck.transform.basis * Vector3.FORWARD).normalized() * SLIDE_BOOST
+		DECELERATION = DECELERATION_SLIDE
+		
+		slide_transition = get_tree().create_tween()
+		slide_transition.tween_property(neck, "position", Vector3(neck.position.x, 0.0, neck.position.z), 0.1)
+		
+		sliding = true
+	elif Input.is_action_just_released("slide"):
+		speed_modifier = 1.0
+		DECELERATION = DECELERATION_DEFAULT
+		slide_transition = get_tree().create_tween()
+		slide_transition.tween_property(neck, "position", Vector3(neck.position.x, 0.609, neck.position.z), 0.1)
+		
+		sliding = false
+
+
 func _physics_process(delta: float) -> void:
 	global.debug.add_property("MovementSpeed", SPEED, 1)
 	global.debug.add_property("Acceleration", ACCELERATION, 1)
@@ -52,9 +75,10 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	
 	# Direction is based on the neck
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction and not sliding:
 		velocity.x = lerp(velocity.x, direction.x * SPEED * speed_modifier, ACCELERATION)
 		velocity.z = lerp(velocity.z, direction.z * SPEED * speed_modifier, ACCELERATION)
 	else:
