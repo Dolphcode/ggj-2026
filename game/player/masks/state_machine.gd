eed06ff5990f18e4a973c4787e884e6ff2f1e1bf
riidans <109998029+riidans@users.noreply.github.com>
2026-02-01 14:39:05 -0500

    Add pixelated/retro shader

--- game/player/masks/state_machine.gd
+++ game/player/masks/state_machine.gd
@@ -6,7 +6,7 @@ class_name MaskHandler
 var current_state : State
 var mask_time : float
 var gun_type : String
-var mask_queue = ["fire_mask"]
+var mask_queue = ["radar_mask"]
 #var mask_queue = []
 var states : Dictionary = {}
 
