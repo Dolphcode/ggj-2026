44cd0650b73f162274addee8390742b42aeba369
jds-472 <jds472@scarletmail.rutgers.edu>
2026-01-31 20:35:24 -0500

    fixed whatever i fixed but added enemy and mask spawning scripts

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,7 @@ class_name Player
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
 
+var current_room:Node3D
 var speed_modifier := 1.0
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -56,3 +57,10 @@ func _physics_process(delta: float) -> void:
 		print("attack")
 
 	move_and_slide()
+	
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, Vector3(global_position.x, global_position.y-10, global_position.z),
+		16, [self])
+	query.collide_with_bodies = true
+	var result = space_state.intersect_ray(query)
+	current_room = result.collider.get_parent()
---
+++ game/map_manager.gd
@@ -0,0 +1,75 @@
+extends Node
+
+@export var difficulty_factor = 1.0
+@export var enemy_spawn_time = 15.0
+@export var mask_spawn_time = 15.0
+
+@export_category("Mask Types")
+@export var fire_mask_obj:PackedScene #whatever class the mask is
+@export var ice_mask_obj:PackedScene #^
+@export var radar_mask_obj:PackedScene #^
+@export var rage_mask_obj:PackedScene #^
+
+@export_category("Enemy Types")
+@export var light_enemy:PackedScene
+@export var heavy_enemy:PackedScene
+@export var lunge_enemy:PackedScene
+@export var dash_enemy:PackedScene
+@export var flylunge_enemy:PackedScene
+@export var flysnipe_enemy:PackedScene
+
+@export_category("Rooms")
+@export var top_left:Node3D
+@export var top_right:Node3D
+@export var bottom_left:Node3D
+@export var bottom_right:Node3D
+@export var center:Node3D
+
+@onready var rooms = [top_left, top_right, bottom_left, bottom_right, center]
+@onready var enemies = [light_enemy, heavy_enemy, lunge_enemy, dash_enemy, flylunge_enemy, flysnipe_enemy]
+@onready var masks = [fire_mask_obj, ice_mask_obj, radar_mask_obj, rage_mask_obj]
+
+enum MaskType {FIRE, ICE, RADAR, RAGE}
+enum EnemyType {LIGHT, HEAVY, LUNGE, DASH, FLYLUNGE, FLYSNIPE}
+enum RoomType{TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT, CENTER}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	%MaskSpawnTimer.wait_time = mask_spawn_time
+	%EnemySpawnTimer.wait_time = enemy_spawn_time
+	randomize()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_enemy_spawn_timer_timeout() -> void:
+	var current_room = rooms.pick_random()
+	while (global.player.current_room == current_room):
+		current_room = rooms.pick_random()
+		print(current_room)
+	var spawn_pos = current_room.get_tree().get_nodes_in_group("spawns").pick_random().global_position
+	var new_enemy = enemies.pick_random().instantiate()
+	new_enemy.position = spawn_pos
+	print("spawned enemy ", new_enemy.name, " at ", spawn_pos)
+	call_deferred("add_child", new_enemy)
+	pass # Replace with function body.
+
+func _on_mask_spawn_timer_timeout() -> void:
+	spawn_masks()
+	pass # Replace with function body.
+	
+func spawn_masks():
+	var count = 0
+	var mask_spawns = get_tree().get_nodes_in_group("mask_spawns")
+	mask_spawns.shuffle()
+	var new_mask
+	for spawn in mask_spawns:
+		new_mask = masks[count].instantiate()
+		new_mask.position = spawn.global_position
+		print("spawned ", masks[count], " at ", spawn.get_parent().name)
+		call_deferred("add_child", new_mask)
+		count += count
+	pass
