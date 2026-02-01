7a501c893f796e3e94e2e35e49a1a344de115a23
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2026-02-01 04:30:14 -0500

    Fixed flamethrower spawn point

--- game/player/flamethrower.gd
+++ game/player/flamethrower.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 var projectile = preload('res://projectile.tscn')
-@export var player_cam : Camera3D
+@export var facing_obj : Node3D
+@export var fire_point: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,13 +12,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("attack"):
+		# Create a projectile
 		var new_instance = projectile.instantiate()
 		new_instance.transform.origin = Vector3(get_parent().global_position.x + randf()/2, get_parent().global_position.y + randf()/2, get_parent().global_position.z + randf()/2)
-		var center = player_cam.get_viewport().get_size() / 2
-		var ray_origin = player_cam.project_ray_origin(center)
-		var ray_direction = player_cam.project_ray_normal(center)
-
-		new_instance.direction = Vector3(ray_direction.x + randf()/2, ray_direction.y + randf()/2, ray_direction.z + randf()/2)
+		
+		# Forward direction based on neck
+		var direction = -facing_obj.global_transform.basis.z
+		
+		# Set the direction of the flamethrower
+		new_instance.direction = Vector3(direction.x + randf()/2, direction.y + randf()/2, direction.z + randf()/2)
+		new_instance.position = fire_point.global_position
+		
+		# Add to the scene
 		get_parent().get_parent().add_child(new_instance)
 			
 #func _physics_process(delta):
