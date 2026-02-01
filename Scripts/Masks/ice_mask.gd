5c7e778a171f5f22914afcb050838f977c55c043
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2026-02-01 06:15:39 -0500

    Was able to get the ice mask to work, fixed the deceleration modifications and added the freezing mechanic

--- Scripts/Masks/ice_mask.gd
+++ Scripts/Masks/ice_mask.gd
@@ -5,14 +5,24 @@ class_name ice_mask
 @export var mask_time = 30
 @export var speed_multiplier = 3.0
 @export var new_fov = 150.0
+
+var old_decel_default: float = 0.0
+var old_decel_slide: float = 0.0
+var old_fov: float = 0.0
+
 #Called on equpping ice mask
 func Enter():
+	old_decel_default = mask.player.DECELERATION_DEFAULT
+	old_decel_slide = mask.player.DECELERATION_SLIDE
+	old_fov = mask.player.get_node("Neck/Camera3D").fov
+	
 	get_parent().mask_time = mask_time #long
 	#gun_type = 'cold gun'
 	mask.player.get_node("FrozenShotgun").process_mode = PROCESS_MODE_INHERIT
 	mask.player.get_node('Shotgun').process_mode = PROCESS_MODE_DISABLED
 	mask.player.speed_modifier = speed_multiplier
-	mask.player.DECELERATION = 0.0
+	mask.player.DECELERATION_DEFAULT = 0.1
+	mask.player.DECELERATION_SLIDE = 0.0
 	mask.player.get_node("Neck/Camera3D").fov = new_fov
 
 	
@@ -21,11 +31,10 @@ func Exit():
 	mask.player.get_node("FrozenShotgun").process_mode = PROCESS_MODE_DISABLED
 	mask.player.get_node('Shotgun').process_mode = PROCESS_MODE_INHERIT
 	mask.player.speed_modifier = 1.0
-	mask.player.DECELERATION = 1.0
-	mask.player.get_node("Neck/Camera3D").fov = 75.0
-	
+	mask.player.DECELERATION_DEFAULT = old_decel_default
+	mask.player.DECELERATION_SLIDE = old_decel_slide
+	mask.player.get_node("Neck/Camera3D").fov = old_fov
+
+
 func Update(delta: float):
 	get_parent().mask_update(delta, self)
-		
-func Physics_update(_delta:float):
-	pass		
