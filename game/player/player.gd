abba9167d4384bdf8252f0fa34f42f5fb482ec0b
Sebastian Vargas <49503850+Dolphcode@users.noreply.github.com>
2026-02-03 16:05:38 -0500

    Fixed sliding reverting speed modifier to 1.0 on release

--- game/player/player.gd
+++ game/player/player.gd
@@ -47,11 +47,11 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * SENSITIVITY)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
-
+var slide_modifier: float = 1.0
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("slide"):
 		AudioManager.get_node("AudioSlide").play()
-		speed_modifier = 0.0
+		slide_modifier = 0.0
 		velocity = (neck.transform.basis * Vector3.FORWARD).normalized() * SLIDE_BOOST
 		DECELERATION = DECELERATION_SLIDE
 		
@@ -64,7 +64,7 @@ func _process(delta: float) -> void:
 		stand_hitbox.set_deferred("disabled", true)
 		sliding = true
 	elif Input.is_action_just_released("slide"):
-		speed_modifier = 1.0
+		slide_modifier = 1.0
 		DECELERATION = DECELERATION_DEFAULT
 		slide_transition = get_tree().create_tween()
 		slide_transition.tween_property(neck, "position", Vector3(neck.position.x, 2.528, neck.position.z), 0.1)
@@ -99,8 +99,8 @@ func _physics_process(delta: float) -> void:
 	if direction.length_squared() != 0 and not sliding:
 		if get_node("Footsteps").playing == false and is_on_floor():
 			get_node("Footsteps").play() #adjust (add more empty space)
-		velocity.x = lerp(velocity.x, direction.x * SPEED * speed_modifier, ACCELERATION)
-		velocity.z = lerp(velocity.z, direction.z * SPEED * speed_modifier, ACCELERATION)
+		velocity.x = lerp(velocity.x, direction.x * SPEED * speed_modifier * slide_modifier, ACCELERATION)
+		velocity.z = lerp(velocity.z, direction.z * SPEED * speed_modifier * slide_modifier, ACCELERATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECELERATION)
 		velocity.z = move_toward(velocity.z, 0, DECELERATION)
